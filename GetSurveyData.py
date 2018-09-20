7b9a410fc200fc790b418ded6a5d9f3c5bda772e
AshpakMulani <42534082+AshpakMulani@users.noreply.github.com>
2018-09-20 23:51:17 +0530

    Check current caller is already present in DB to decide new or return caller.

--- GetSurveyData.py
+++ GetSurveyData.py
@@ -1,6 +1,7 @@
 import boto3
 from botocore.exceptions import ClientError
 
+
 def lambda_handler(event, context):
     '''
     DOCSTRING:  This function is responsible for checking current caller has already completed survey by checking
